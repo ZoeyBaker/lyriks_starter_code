c06e25e404b6868fdcc072443a068180ba461804
Zoey Baker <zoeybaker625@gmail.com>
2023-10-31 12:30:11 -0500

    initial commit

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
+
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 };
-=======
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
->>>>>>> 57dc928053f503eaedb2229c704ecdf68742c00c
+
